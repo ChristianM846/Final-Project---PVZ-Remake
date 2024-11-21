0b6e8df88a4a24359b3283ebce506e45fe793590
Christian Moyes <339463846@GoTVDSB.ca>
2024-11-21 09:25:48 -0500

    Making Intro Screen

--- Final Project - PVZ Remake/Game1.cs
+++ Final Project - PVZ Remake/Game1.cs
@@ -11,15 +11,25 @@ namespace Final_Project___PVZ_Remake
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        int sun;
+
         MouseState mouseState, prevMouseState;
         KeyboardState keyboardState;
 
         Texture2D titleScreen;
+        Texture2D frontYard;
+        Texture2D plantRoster;
 
         Rectangle window;
+        Rectangle plantRosterRect;
+
+        Vector2 sunBankLocation;
 
         SoundEffect introTheme;
         SoundEffectInstance introThemeInstance;
+
+        SpriteFont titleFont;
+        SpriteFont sunFont;
 
         Screen screen;
 
@@ -48,6 +58,9 @@ namespace Final_Project___PVZ_Remake
         {
             screen = Screen.Title;
             window = new Rectangle(0, 0, 800, 520);
+            sun = 50;
+
+            plantRosterRect = new Rectangle(200, 2, 450, 70);
 
             base.Initialize();
         }
@@ -58,6 +71,12 @@ namespace Final_Project___PVZ_Remake
 
             //TitleScreen
             titleScreen = Content.Load<Texture2D>("Images/TitleScreen");
+            titleFont = Content.Load<SpriteFont>("Fonts/TitleFont2");
+
+            //Game
+            frontYard = Content.Load<Texture2D>("Images/FrontYard");
+            plantRoster = Content.Load<Texture2D>("Images/PlantRoster");
+            sunFont = Content.Load<SpriteFont>("Fonts/SunFontSP");
 
             //SoundEffects
             introTheme = Content.Load<SoundEffect>("Sounds/IntroTheme");
@@ -80,16 +99,42 @@ namespace Final_Project___PVZ_Remake
                     introThemeInstance.Play();
                 }
 
-                if (mouseState.LeftButton == ButtonState.Pressed && prevMouseState.LeftButton == ButtonState.Released)
+                if (mouseState.LeftButton == ButtonState.Pressed && prevMouseState.LeftButton == ButtonState.Released && window.Contains(mouseState.Position))
                 {
                     screen = Screen.Intro;
-                    introThemeInstance.Stop();
                 }
             }
             else if (screen == Screen.Intro)
             {
 
             }
+
+
+            if (sun == 0)
+            {
+                sunBankLocation = new Vector2(224, 53);
+            }
+            else if (sun > 0 && sun < 100)
+            {
+                sunBankLocation = new Vector2(221, 53);
+            }
+            else if (sun >= 100 && sun < 200)
+            {
+                sunBankLocation = new Vector2(218, 53);
+            }
+            else if (sun >= 200 && sun < 1000)
+            {
+                sunBankLocation = new Vector2(216, 53);
+            }
+            else if (sun >= 1000 && sun < 2000)
+            {
+                sunBankLocation = new Vector2(213, 53);
+            }
+            else
+            {
+                sunBankLocation = new Vector2(210, 53);
+            }
+
 
 
             prevMouseState = Mouse.GetState();
@@ -112,10 +157,13 @@ namespace Final_Project___PVZ_Remake
             if (screen == Screen.Title)
             {
                 _spriteBatch.Draw(titleScreen, window, Color.White);
+                _spriteBatch.DrawString(titleFont, "A Jank Remake", new Vector2(100, 100), Color.Black);
             }
             else if (screen == Screen.Intro)
             {
-
+                _spriteBatch.Draw(frontYard, window, Color.White);
+                _spriteBatch.Draw(plantRoster, plantRosterRect, Color.White);
+                _spriteBatch.DrawString(sunFont, $"{sun}", sunBankLocation, Color.Black);
             }
 
 
